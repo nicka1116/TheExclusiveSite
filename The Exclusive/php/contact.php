367f6959fdbd29999a5df70a345f91b422de763e
Nick Allen <nickallen@nicks-mbp.wcasd.k12.pa.us>
2017-09-26 13:34:40 -0400

    PHP update to include contact form on contact and venues pages.

--- The Exclusive/php/contact.php
+++ The Exclusive/php/contact.php
@@ -43,8 +43,8 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$from = $email;
-	$to = 'info@example.com';  // please change this email id
-	$subject = 'Contact Form : Titan - The best downloaded template ever';
+	$to = 'info@theexclusivemusic.com';  // please change this email id
+	$subject = 'Hello';
 
 	$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
